68fb19cbb1996edb5813c07c40aba0463ee35d08
pjsinco <psinco@fas.harvard.edu>
2015-10-15 15:34:16 -0500

    Config config.rb

--- config.rb
+++ config.rb
@@ -1,12 +1,9 @@
-require 'susy'
-require 'breakpoint'
-require 'compass-normalize'
 http_path = '/'
 css_dir = '.'
 fonts_dir = 'fonts'
 sass_dir = 'sass'
 javascripts_dir = 'js'
 output_style = :compressed
-images_dir = 'img'
+images_dir = 'images'
 relative_assets = true
 line_comments = true
